4e59cab96afa425b481b022add25d7d580fa7989
ahmettaliicann@gmail.com <ahmettaliicann@gmail.com>
2021-03-08 19:46:13 +0300

    anasayfa

--- Emlak/Controllers/HomeController.cs
+++ Emlak/Controllers/HomeController.cs
@@ -1,17 +1,24 @@
-﻿using System;
+﻿using Emlak.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
 
 namespace Emlak.Controllers
 {
     public class HomeController : Controller
     {
+        DataContext db = new DataContext();
         // GET: Home
         public ActionResult Index()
         {
-            return View();
+            var imgs = db.Resims.ToList();
+            ViewBag.imgs = imgs;
+
+            var ilan = db.Ilans.Include(m => m.Mahalle).Include(e => e.Tip);
+            return View(ilan.ToList());
         }
 
         public ActionResult Detay()
